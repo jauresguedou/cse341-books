ba1cb125189c418fdf1044f703bdbf4ff7b2e160
Jaures Guedou <sewlanguedou@gmail.com>
2026-09-01 14:44:54 +0100

    Add Express application files

---
+++ server.js
@@ -0,0 +1,10 @@
+import app from './app.js';
+
+const PORT = process.env.PORT;
+if (!PORT) {
+    throw new Error('PORT is not defined.Make sure your local npm scripts reference the .env file with --env-file=.env, or define PORT in your hosted environment settings.');
+};
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
